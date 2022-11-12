9cc04857d5e5dbd9a1e764d58c0b514e6c390163
Masataka Koguchi <bedauxx@gmail.com>
2022-11-13 01:02:51 +0900

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import { VFXProvider } from 'react-vfx'
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <VFXProvider><Component {...pageProps} /></VFXProvider>
+  return <VFXProvider><Component {...pageProps} /><Analytics /></VFXProvider>
 }
 
 export default MyApp
