653163ff867fa3d524e4d09d92b9002e872f43bc
bedauxx <bedauxx+1@gmail.com>
2022-11-14 11:47:19 +0900

    add salmonrun

---
+++ component/CoopStages.js
@@ -0,0 +1,141 @@
+import styles from '../styles/DefaultStages.module.css'
+import Moment from 'react-moment'
+import Image from 'next/image'
+
+export default function CoopStages({ salmonSchedule }) {
+  return (
+    <>
+    <div className={styles.entry}>
+        <h2>サーモンラン</h2>
+        <h2>Current Stages</h2>
+        <h3 className='stage-schedule'>
+            <Moment format="YY/MM/DD H:mm">
+                {salmonSchedule.results[0].start_time}
+            </Moment>
+            {' '}〜{' '}
+            <Moment format="YY/MM/DD H:mm">
+                {salmonSchedule.results[0].end_time}
+            </Moment>
+        </h3>
+
+        <div className={styles.rules}>
+            <div>
+            <h3>
+            {salmonSchedule.results[0].stage.name}
+            </h3>
+            <Image
+            src={salmonSchedule.results[0].stage.image}
+            width={128}
+            height={64}
+            alt="stage-pic"
+            />
+            <ul className={styles.weaponList}>
+                <li>
+                    <p>{salmonSchedule.results[0].weapons[0].name}</p>
+                    <Image
+                    src={salmonSchedule.results[0].weapons[0].image}
+                    width={128}
+                    height={128}
+                    alt="weapons-pic"
+                    />
+                </li>
+                <li>
+                    <p>{salmonSchedule.results[0].weapons[1].name}</p>
+                    <Image
+                    src={salmonSchedule.results[0].weapons[1].image}
+                    width={128}
+                    height={128}
+                    alt="weapons-pic"
+                    />
+                </li>
+                <li>
+                    <p>{salmonSchedule.results[0].weapons[2].name}</p>
+                    <Image
+                    src={salmonSchedule.results[0].weapons[2].image}
+                    width={128}
+                    height={128}
+                    alt="weapons-pic"
+                    />
+                </li>
+                <li>
+                    <p>{salmonSchedule.results[0].weapons[3].name}</p>
+                    <Image
+                    src={salmonSchedule.results[0].weapons[3].image}
+                    width={128}
+                    height={128}
+                    alt="weapons-pic"
+                    />
+                </li>
+            </ul>
+            </div>
+        </div>
+    </div>
+
+    <div className={styles.entry}>
+            <h2>サーモンラン</h2>
+            <h2>Next Stages</h2>
+            <h3 className='stage-schedule'>
+                <Moment format="YY/MM/DD H:mm">
+                    {salmonSchedule.results[0].start_time}
+                </Moment>
+                {' '}〜{' '}
+                <Moment format="YY/MM/DD H:mm">
+                    {salmonSchedule.results[0].end_time}
+                </Moment>
+            </h3>
+
+            <div className={styles.rules}>
+                <div>
+                <h3>
+                {salmonSchedule.results[1].stage.name}
+                </h3>
+                <Image
+                src={salmonSchedule.results[1].stage.image}
+                width={128}
+                height={64}
+                alt="stage-pic"
+                />
+                <ul className={styles.weaponList}>
+                    <li>
+                        <p>{salmonSchedule.results[1].weapons[0].name}</p>
+                        <Image
+                        src={salmonSchedule.results[1].weapons[0].image}
+                        width={128}
+                        height={128}
+                        alt="weapons-pic"
+                        />
+                    </li>
+                    <li>
+                        <p>{salmonSchedule.results[1].weapons[1].name}</p>
+                        <Image
+                        src={salmonSchedule.results[1].weapons[1].image}
+                        width={128}
+                        height={128}
+                        alt="weapons-pic"
+                        />
+                    </li>
+                    <li>
+                        <p>{salmonSchedule.results[1].weapons[2].name}</p>
+                        <Image
+                        src={salmonSchedule.results[1].weapons[2].image}
+                        width={128}
+                        height={128}
+                        alt="weapons-pic"
+                        />
+                    </li>
+                    <li>
+                        <p>{salmonSchedule.results[1].weapons[3].name}</p>
+                        <Image
+                        src={salmonSchedule.results[1].weapons[3].image}
+                        width={128}
+                        height={128}
+                        alt="weapons-pic"
+                        />
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,11 @@ import { VFXSpan } from 'react-vfx'
 import styles from '../styles/Home.module.css'
 import CurrentStages from '../component/CurrentStages.js'
 import NextStages from '../component/NextStages.js'
+import CoopStages from '../component/CoopStages.js'
 import QR from '../component/qr.js'
 import { GetStaticProps } from 'next'
 
-export default function Home({schedule}) {
+export default function Home({schedule,salmonSchedule}) {
   return (
     <div className={styles.container}>
       <Head prefix='og:http://ogp.me/ns#'>
@@ -31,6 +32,8 @@ export default function Home({schedule}) {
         </h1>
             <CurrentStages schedule={schedule}></CurrentStages>
             <NextStages schedule={schedule}></NextStages>
+            <CoopStages salmonSchedule={salmonSchedule}></CoopStages>
+                
         <QR></QR>
         
       </main>
@@ -44,7 +47,6 @@ export default function Home({schedule}) {
 }
 
 export const getStaticProps = async() => {
-// export async function getStaticProps() でも同じ
   const req = new Request("https://spla3.yuu26.com/api/schedule", {
     headers: {
       "user-agent": "S3/1.0(twitter@missplatoon)",
@@ -52,10 +54,16 @@ export const getStaticProps = async() => {
   });
   const res = await fetch(req)
   const schedule = await res.json()
-  //console.log("-----------------------------------------------------------------------------")
-  //console.log(schedule.result.regular[0])
+
+  const salmon_req = new Request("https://spla3.yuu26.com/api/coop-grouping-regular/schedule", {
+    headers: {
+      "user-agent": "S3/1.0(twitter@missplatoon)",
+    },
+  });
+  const salmon_res = await fetch(salmon_req)
+  const salmonSchedule = await salmon_res.json()
 
   return {
-    props: { schedule }, // will be passed to the page component as props
+    props: { schedule,salmonSchedule }, // will be passed to the page component as props
   }
 }
