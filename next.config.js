48a39f82910f2f9dfb248a2fd69b155b37b48944
bedauxx <bedauxx+1@gmail.com>
2022-11-21 14:29:58 +0900

    add pwa

--- next.config.js
+++ next.config.js
@@ -8,3 +8,12 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const withPWA = require("next-pwa")({dest: 'public'});
+
+module.exports = withPWA({
+    reactStrinctMode: true,
+    images: {
+      domains: ['api.lp1.av5ja.srv.nintendo.net'],
+    },
+  });
