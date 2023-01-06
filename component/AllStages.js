0f9bb31044ec4ae1edf858121c4f7f1978fece6a
Masataka Koguchi <bedauxx@gmail.com>
2023-01-07 03:17:08 +0900

    edit all - fest mod

--- component/AllStages.js
+++ component/AllStages.js
@@ -16,143 +16,156 @@ export default function AllStages({ schedule }) {
       <Tab.Panels>
         <Tab.Panel>
             <ul>
-                {schedule.result.regular.map(({stages,start_time,end_time}) =>
+                {schedule.result.regular.map(({stages,start_time,end_time,is_fest}) =>
                     <li>
-                        <div className={styles.entry}>
-                            <h3 className='stage-schedule'>
-                                <Moment format="MM/DD H:mm">{start_time}</Moment>
-                                {' '}〜{' '}
-                                <Moment format="MM/DD H:mm">{end_time}</Moment>
-                            </h3>
-                            <ul className={styles.stageList}>
-                                <li>
-                                <p>{stages[0].name}</p>
-                                <Image
-                                    src={stages[0].image}
-                                    width={128}
-                                    height={64}
-                                    alt="stage-pic"
-                                />
-                                </li>
-                                <li>
-                                <p>{stages[1].name}</p>
-                                <Image
-                                    src={stages[1].image}
-                                    width={128}
-                                    height={64}
-                                    alt="stage-pic"
-                                    />
-                                </li>
-                            </ul>
-                        </div>
+                              {is_fest
+                                    ? <div></div>
+                                    : <div className={styles.entry}>
+                                    <h3 className='stage-schedule'>
+                                        <Moment format="MM/DD H:mm">{start_time}</Moment>
+                                        {' '}〜{' '}
+                                        <Moment format="MM/DD H:mm">{end_time}</Moment>
+                                    </h3>
+                                    <ul className={styles.stageList}>
+                                        <li>
+                                        <p>{stages[0].name}</p>
+                                        <Image
+                                            src={stages[0].image}
+                                            width={128}
+                                            height={64}
+                                            alt="stage-pic"
+                                        />
+                                        </li>
+                                        <li>
+                                        <p>{stages[1].name}</p>
+                                        <Image
+                                            src={stages[1].image}
+                                            width={128}
+                                            height={64}
+                                            alt="stage-pic"
+                                            />
+                                        </li>
+                                    </ul>
+                                </div>
+                                }
+
                     </li>
                 )}
             </ul>
         </Tab.Panel>
         <Tab.Panel>
             <ul>
-                {schedule.result.bankara_challenge.map(({rule, stages,start_time,end_time}) =>
+                {schedule.result.bankara_challenge.map(({rule, stages,start_time,end_time,is_fest}) =>
                     <li>
-                        <div className={styles.entry}>
-                            <h3 className='stage-schedule'>
-                                <Moment format="MM/DD H:mm">{start_time}</Moment>
-                                {' '}〜{' '}
-                                <Moment format="MM/DD H:mm">{end_time}</Moment>
-                            </h3>
-                            <h3>{rule.name}</h3>
-                            <ul className={styles.stageList}>
-                                <li>
-                                <p>{stages[0].name}</p>
-                                <Image
-                                    src={stages[0].image}
-                                    width={128}
-                                    height={64}
-                                    alt="stage-pic"
-                                />
-                                </li>
-                                <li>
-                                <p>{stages[1].name}</p>
-                                <Image
-                                    src={stages[1].image}
-                                    width={128}
-                                    height={64}
-                                    alt="stage-pic"
-                                    />
-                                </li>
-                            </ul>
-                        </div>
+                    {is_fest
+                                    ? <div></div>
+                                    : <div className={styles.entry}>
+                                        <h3 className='stage-schedule'>
+                                            <Moment format="MM/DD H:mm">{start_time}</Moment>
+                                            {' '}〜{' '}
+                                            <Moment format="MM/DD H:mm">{end_time}</Moment>
+                                        </h3>
+                                        <h3>{rule.name}</h3>
+                                        <ul className={styles.stageList}>
+                                            <li>
+                                            <p>{stages[0].name}</p>
+                                            <Image
+                                                src={stages[0].image}
+                                                width={128}
+                                                height={64}
+                                                alt="stage-pic"
+                                            />
+                                            </li>
+                                            <li>
+                                            <p>{stages[1].name}</p>
+                                            <Image
+                                                src={stages[1].image}
+                                                width={128}
+                                                height={64}
+                                                alt="stage-pic"
+                                                />
+                                            </li>
+                                        </ul>
+                                    </div>
+                    }
                     </li>
                 )}
             </ul>
         </Tab.Panel>
         <Tab.Panel>
             <ul>
-                {schedule.result.bankara_open.map(({rule, stages,start_time,end_time}) =>
+                {schedule.result.bankara_open.map(({rule, stages,start_time,end_time,is_fest}) =>
                     <li>
-                        <div className={styles.entry}>
-                            <h3 className='stage-schedule'>
-                                <Moment format="MM/DD H:mm">{start_time}</Moment>
-                                {' '}〜{' '}
-                                <Moment format="MM/DD H:mm">{end_time}</Moment>
-                            </h3>
-                            <h3>{rule.name}</h3>
-                            <ul className={styles.stageList}>
-                                <li>
-                                <p>{stages[0].name}</p>
-                                <Image
-                                    src={stages[0].image}
-                                    width={128}
-                                    height={64}
-                                    alt="stage-pic"
-                                />
-                                </li>
-                                <li>
-                                <p>{stages[1].name}</p>
-                                <Image
-                                    src={stages[1].image}
-                                    width={128}
-                                    height={64}
-                                    alt="stage-pic"
-                                    />
-                                </li>
-                            </ul>
-                        </div>
+                    {is_fest
+                                    ? <div></div>
+                                    : <div className={styles.entry}>
+                                    <h3 className='stage-schedule'>
+                                        <Moment format="MM/DD H:mm">{start_time}</Moment>
+                                        {' '}〜{' '}
+                                        <Moment format="MM/DD H:mm">{end_time}</Moment>
+                                    </h3>
+                                    <h3>{rule.name}</h3>
+                                    <ul className={styles.stageList}>
+                                        <li>
+                                        <p>{stages[0].name}</p>
+                                        <Image
+                                            src={stages[0].image}
+                                            width={128}
+                                            height={64}
+                                            alt="stage-pic"
+                                        />
+                                        </li>
+                                        <li>
+                                        <p>{stages[1].name}</p>
+                                        <Image
+                                            src={stages[1].image}
+                                            width={128}
+                                            height={64}
+                                            alt="stage-pic"
+                                            />
+                                        </li>
+                                    </ul>
+                                </div>
+                    }
                     </li>
                 )}
             </ul>
         </Tab.Panel>
         <Tab.Panel>
             <ul>
-                {schedule.result.x.map(({rule, stages,start_time,end_time}) =>
+                {schedule.result.x.map(({rule, stages,start_time,end_time,is_fest}) =>
                     <li>
-                        <div className={styles.entry}>
-                            <h3 className='stage-schedule'>
-                                <Moment format="MM/DD H:mm">{start_time}</Moment>
-                                {' '}〜{' '}
-                                <Moment format="MM/DD H:mm">{end_time}</Moment>
-                            </h3>
-                            <h3>{rule.name}</h3>
-                            <ul className={styles.stageList}>
-                                <li>
-                                <p>{stages[0].name}</p>
-                                <Image
-                                    src={stages[0].image}
-                                    width={128}
-                                    height={64}
-                                    alt="stage-pic"
-                                />
-                                </li>
-                                <li>
-                                <p>{stages[1].name}</p>
-                                <Image
-                                    src={stages[1].image}
-                                    width={128}
-                                    height={64}
-                                    alt="stage-pic"
-                                    />
-                                </li>
-                            </ul>
-                        </div>
+                    {is_fest
+                                    ? <div></div>
+                                    : <div className={styles.entry}>
+                                    <h3 className='stage-schedule'>
+                                        <Moment format="MM/DD H:mm">{start_time}</Moment>
+                                        {' '}〜{' '}
+                                        <Moment format="MM/DD H:mm">{end_time}</Moment>
+                                    </h3>
+                                    <h3>{rule.name}</h3>
+                                    <ul className={styles.stageList}>
+                                        <li>
+                                        <p>{stages[0].name}</p>
+                                        <Image
+                                            src={stages[0].image}
+                                            width={128}
+                                            height={64}
+                                            alt="stage-pic"
+                                        />
+                                        </li>
+                                        <li>
+                                        <p>{stages[1].name}</p>
+                                        <Image
+                                            src={stages[1].image}
+                                            width={128}
+                                            height={64}
+                                            alt="stage-pic"
+                                            />
+                                        </li>
+                                    </ul>
+                                </div>
+                            }
                     </li>
                 )}
             </ul>
